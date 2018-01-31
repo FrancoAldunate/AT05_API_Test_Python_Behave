81d1461513536855784e24d637f0b33cff7d0b19
Franco Daniel Aldunate Cordero <Franco.Aldunate@fundacion-jala.org>
2018-01-31 10:32:32 -0400

    Adding background exercise from Behave_4 pptx

---
+++ FrancoAldunate/class_exercises/test/features/steps/background_steps.py
@@ -0,0 +1,46 @@
+# Background: Go to My profile
+@given(u'I login application')
+def step_impl(context):
+    a = 1
+
+
+@when(u'I click on User icon')
+def step_impl(context):
+    a = 1
+
+
+@then(u'I see My profile')
+def step_impl(context):
+    a = 1
+
+
+# Scenario: Change user picture
+@given(u'I select change picture')
+def step_impl(context):
+    a = 1
+
+
+@when(u'I select a new image from my pc')
+def step_impl(context):
+    a = 1
+
+
+@then(u'I see new picture loaded')
+def step_impl(context):
+    a = 1
+
+
+# Scenario: Change user password
+@given(u'I select change password')
+def step_impl(context):
+    a = 1
+
+
+@when(u'I save my new password')
+def step_impl(context):
+    a = 1
+
+
+@then(u'I should see confirmation message')
+def step_impl(context):
+    a = 1
